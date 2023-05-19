6b4d4cf9103e14caa89ec9603123fd117bd5dc83
Pam-Devil <111209178+Pam-Devil@users.noreply.github.com>
2023-05-19 16:42:37 -0300

    2 files modified, 3 added

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title: string = 'My first blog with SvelteKit';
+export const description: string = 'A simple blog made quickly using SvelteKit and open-props';
+export const url: any = dev ? 'https://localhost:5173' : 'https://myblog.com';
